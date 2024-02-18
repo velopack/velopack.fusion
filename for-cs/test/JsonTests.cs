d36ef2033b32237278719486dfea2ab6287ec53d
Caelan Sayler <git@caesay.com>
2024-02-18 15:57:04 +0000

    Add more json tests

--- for-cs/test/JsonTests.cs
+++ for-cs/test/JsonTests.cs
@@ -41,6 +41,8 @@ namespace CsTests
         [InlineData("valid-0011.json")]
         [InlineData("valid-0012.json")]
         [InlineData("valid-0013.json")]
+        [InlineData("valid-0014.json")]
+        [InlineData("valid-0015.json")]
         [InlineData("ext-valid-0000.json")]
         [InlineData("ext-valid-0001.json")]
         [InlineData("ext-valid-0002.json")]
@@ -53,19 +55,28 @@ namespace CsTests
             Velopack.JsonNode.Parse(json);
         }
 
-        //[Fact]
-        //public void InvalidExtThrows()
-        //{
-        //    var dir = Fixtures.GetFixturesDir();
+        [Fact]
+        public void ParsesExponents()
+        {
+            var json = "{ \"min\": -1.0e+28, \"max\": 1.0e+28 }";
+            var node = Velopack.JsonNode.Parse(json);
 
-        //    foreach (var f in Directory.EnumerateFiles(dir, "*.json"))
-        //    {
-        //        if (f.Contains(f) && !Path.GetFileName(f).StartsWith("invalid-"))
-        //        {
-        //            var json = File.ReadAllText(f);
-        //            Assert.Throws<Velopack.JsonParseException>(() => Velopack.JsonNode.Parse(json));
-        //        }
-        //    }
-        //}
+            Assert.Equal(-1.0e+28, node.AsObject()["min"].AsNumber());
+            Assert.Equal(1.0e+28, node.AsObject()["max"].AsNumber());
+        }
+
+        [Fact]
+        public void ParsesObjectCorrectly()
+        {
+            var json = "{\"status\": \"ok\", \"results\": [{\"recordings\": [{\"id\": \"889ec8e0-b8a6-4ff1-a104-5512ea49fe87\"}], /* block comment */ \"score\": 0.879051, \"id\": \"45047cb1-3d3f-477e-a3dc-f14e8254e78d\"}]}\r\n";
+            var node = Velopack.JsonNode.Parse(json);
+            Assert.Equal("ok", node.AsObject()["status"].AsString());
+            var results = node.AsObject()["results"].AsArray();
+            var result1 = results[0].AsObject();
+            Assert.Equal(0.879051, result1["score"].AsNumber());
+            Assert.Equal("45047cb1-3d3f-477e-a3dc-f14e8254e78d", result1["id"].AsString());
+            var recordings = result1["recordings"].AsArray();
+            Assert.Equal("889ec8e0-b8a6-4ff1-a104-5512ea49fe87", recordings[0].AsObject()["id"].AsString());
+        }
     }
 }
