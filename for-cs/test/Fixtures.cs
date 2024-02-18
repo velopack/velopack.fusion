56218e888e38387e572e2ce2bfcf8ed74910df2b
Caelan Sayler <git@caesay.com>
2024-02-18 15:04:26 +0000

    add json tests

---
+++ for-cs/test/Fixtures.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+namespace CsTests
+{
+    public class Fixtures
+    {
+        public static string GetFixturesDir()
+        {
+            return Path.GetFullPath(Assembly.GetExecutingAssembly()
+                .GetCustomAttributes<AssemblyMetadataAttribute>()
+                .Where(x => x.Key == "FixturesDir")
+                .Single().Value);
+        }
+    }
+}
---
+++ for-cs/test/JsonTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace CsTests
+{
+    public class JsonTests
+    {
+        [Fact]
+        public void InvalidBasicThrows()
+        {
+            var dir = Fixtures.GetFixturesDir();
+
+            foreach (var f in Directory.EnumerateFiles(dir, "*.json"))
+            {
+                if (Path.GetFileName(f).StartsWith("invalid-"))
+                {
+                    var json = File.ReadAllText(f);
+                    Assert.Throws<Velopack.JsonParseException>(() => Velopack.JsonNode.Parse(json));
+                }
+            }
+        }
+
+        [Fact]
+        public void InvalidExtThrows()
+        {
+            var dir = Fixtures.GetFixturesDir();
+
+            foreach (var f in Directory.EnumerateFiles(dir, "*.json"))
+            {
+                if (f.Contains(f) && !Path.GetFileName(f).StartsWith("invalid-"))
+                {
+                    var json = File.ReadAllText(f);
+                    Assert.Throws<Velopack.JsonParseException>(() => Velopack.JsonNode.Parse(json));
+                }
+            }
+        }
+    }
+}
